0eab49e6855bb17ffb5446c953d03a23b714b21c
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-07-12 19:50:31 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,26 @@
+package basic;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		int tech = 2;
+		
+		switch(tech) {
+		
+			case 0:{
+				System.out.println("Java");
+				break;
+			}
+			case 1:{
+				System.out.println("ES6");
+				break;
+			}
+			case 2:{
+				System.out.println("Not listed..");
+				break;
+			}
+		}
+
+	}
+
+}
