cc7baf6189efe84218303a8e316b2373442b1186
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-07-12 20:36:19 +0530

    Add files via upload

---
+++ week2/FindAreaofRectangle.java
@@ -0,0 +1,24 @@
+package basic;
+
+import java.util.Scanner;
+
+public class FindAreaofRectangle {
+
+	public static void main(String[] args) {
+		   
+	       Scanner scanner = new Scanner(System.in);
+	       System.out.println("Enter the length of rectangle: ");
+	        double length = scanner.nextDouble();
+	         
+	        System.out.println("Enter the width of rectangle: ");
+	        double width = scanner.nextDouble();
+	        
+	        //Area of reactangle
+	        double area = length * width;
+	        System.out.println("Area of Rectangle is: " + area);
+	       
+	       
+
+	}
+
+}
