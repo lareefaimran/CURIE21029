a8a421cbe3011459c2d081251951f1a1042cfebb
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-07-14 21:06:20 +0530

    Add files via upload

---
+++ week2/CheckPrime.java
@@ -0,0 +1,31 @@
+package basic;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		int temp;
+		boolean isPrime = true;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num= scan.nextInt();
+		scan.close();
+		
+		for(int i = 2;i <= num/2;i++) {
+			temp = num % i;
+			if(temp == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		//If isPrime is true then the number is prime else not
+		if(isPrime) {
+			System.out.println(num + " is a Prime number ");
+		}else {
+			System.out.println(num + " is not a Prime number ");
+		}
+		
+	}
+
+}
