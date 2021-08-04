aa0512a3eb123063e3db2fd4d6ff24d8160f5f86
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-08-04 21:04:57 +0530

    Add files via upload

---
+++ week2/ArrayDemo2.java
@@ -0,0 +1,18 @@
+
+public class ArrayDemo2 {
+		public static void main(String[] args) {
+			ArrayDemo2 obj = new ArrayDemo2();
+			obj.met();
+			
+		}
+		int ins;
+		int[] insarr = new int[5];
+		public void met() {
+			int loc=0;
+			int [] locinsarr = new int[5];
+			System.out.println(loc);
+			System.out.println(ins);
+			System.out.println(insarr[1]);
+			System.out.println(locinsarr[1]);
+		}
+}
