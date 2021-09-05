615543d64f00b0facfe32bce19dc36b387d9a831
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-09-05 20:28:43 +0530

    Add files via upload

---
+++ week2/Breakdemo.java
@@ -0,0 +1,21 @@
+package week2;
+
+public class Breakdemo {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+			a:{
+				if(i%2!=0) {
+					break;//breaks the for loop
+					//continue ;//it will continue with the for loop
+				}
+			}
+			System.out.println(i);
+			System.out.println(i);
+			System.out.println(i);
+		}
+	}
+
+	}
+
+
