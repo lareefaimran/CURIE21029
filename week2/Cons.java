1025fd98d1fa461fba809be3296752c9fcd4152f
lareefaimran <48053004+lareefaimran@users.noreply.github.com>
2021-09-05 19:11:33 +0530

    Add files via upload

---
+++ week2/Cons.java
@@ -0,0 +1,16 @@
+package week3lab;
+
+public class Cons {
+	public static void main(String[] args) {
+		SuperClass obj = new SuperClass(1000);
+		
+	
+		
+	}
+
+}
+class SuperClass{
+	SuperClass(int i ) {
+		System.out.println("constructor with paramter..."+i);
+	}
+}
